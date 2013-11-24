8445652714861d61bb13f01d207c68a4cd88deaa
unknown <BorisMor@ngs.ru>
2013-11-24 14:47:29 +0700

    Доделка

--- src/com/sibext/volleyexample/MainActivity.java
+++ src/com/sibext/volleyexample/MainActivity.java
@@ -1,6 +1,8 @@
 package com.sibext.volleyexample;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,11 +12,13 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,12 +29,16 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.sibext.volleyexample.GamePost.OnDrawGamePost;
 
-public class MainActivity extends Activity implements OnClickListener {
-    private int direction = 1;
+public class MainActivity extends Activity implements OnClickListener, OnDrawGamePost {
+    // private int direction = 1;
     private Button butStart;    
     private TextView resGame;
+    private Button butStartFull;
+    private EditText edNumPause;
     
+    /** Результат игры  */
     private Integer resGame0 = 0;
     private Integer resGame1 = 0;
     
@@ -44,71 +52,26 @@ public class MainActivity extends Activity implements OnClickListener {
     private ArrayList<ImageView> feild_place_0;
     private ArrayList<ImageView> feild_place_1;
     
-    // Положения
-    final int PLACE_OUT		= 0;
-    final int PLACE_LEFT	= 1;
-    final int PLACE_CENTER	= 2;
-    final int PLACE_RIGHT	= 3;
-    final int PLACE_NEAR	= 4;
-    
-    // Результат удара
-    final int RESULT_CAUGHT = 0;
-    final int RESULT_GOAL	= 1;
-    final int RESULT_OUT	= 2;
+    /** До скальки идет игра */
+    final int MAX_SCORE = 10; 
+
     
     private void SetFeildPlace(){
     	this.feild_place_0 = new ArrayList<ImageView>(5);  
-    	this.feild_place_0.add(PLACE_OUT,	(ImageView)findViewById(R.id.out0_center));
-    	this.feild_place_0.add(PLACE_LEFT,	(ImageView)findViewById(R.id.f0_left));
-    	this.feild_place_0.add(PLACE_CENTER,(ImageView)findViewById(R.id.f0_centr));
-    	this.feild_place_0.add(PLACE_RIGHT,	(ImageView)findViewById(R.id.f0_right));
-    	this.feild_place_0.add(PLACE_NEAR,	(ImageView)findViewById(R.id.greed21));
+    	this.feild_place_0.add(GamePost.PLACE_OUT,	(ImageView)findViewById(R.id.out0_center));
+    	this.feild_place_0.add(GamePost.PLACE_LEFT,	(ImageView)findViewById(R.id.f0_left));
+    	this.feild_place_0.add(GamePost.PLACE_CENTER,(ImageView)findViewById(R.id.f0_centr));
+    	this.feild_place_0.add(GamePost.PLACE_RIGHT,	(ImageView)findViewById(R.id.f0_right));
+    	this.feild_place_0.add(GamePost.PLACE_NEAR,	(ImageView)findViewById(R.id.greed21));
     	
     	this.feild_place_1 = new ArrayList<ImageView>(5);  
-    	this.feild_place_1.add(PLACE_OUT,	(ImageView)findViewById(R.id.out1_centr));
-    	this.feild_place_1.add(PLACE_LEFT,	(ImageView)findViewById(R.id.f1_left));
-    	this.feild_place_1.add(PLACE_CENTER,(ImageView)findViewById(R.id.f1_centr));
-    	this.feild_place_1.add(PLACE_RIGHT,	(ImageView)findViewById(R.id.f1_right));
-    	this.feild_place_1.add(PLACE_NEAR,	(ImageView)findViewById(R.id.greed21));    	
+    	this.feild_place_1.add(GamePost.PLACE_OUT,	(ImageView)findViewById(R.id.out1_centr));
+    	this.feild_place_1.add(GamePost.PLACE_LEFT,	(ImageView)findViewById(R.id.f1_left));
+    	this.feild_place_1.add(GamePost.PLACE_CENTER,(ImageView)findViewById(R.id.f1_centr));
+    	this.feild_place_1.add(GamePost.PLACE_RIGHT,	(ImageView)findViewById(R.id.f1_right));
+    	this.feild_place_1.add(GamePost.PLACE_NEAR,	(ImageView)findViewById(R.id.greed21));    	
     }
     
-    private int GetIntPlace(String strPlace)
-    {
-    	int result = 0;
-    	
-    	if(strPlace.equals("out"))
-    		result = PLACE_OUT;
-    	else
-    	if(strPlace.equals("left"))
-    		result = PLACE_LEFT;
-    	else
-    	if(strPlace.equals("center"))
-    		result = PLACE_CENTER;
-    	else
-    	if(strPlace.equals("right"))
-    		result = PLACE_RIGHT;
-    	else
-    	if(strPlace.equals("near grid"))
-    		result = PLACE_NEAR;
-    	    	
-		return result;    	
-    }
-    
-    private int GetIntResult(String strResult)
-    {
-    	int result = 0;
-    	
-    	if(strResult.equals("caught"))
-    		result = RESULT_CAUGHT;
-    	else
-    	if(strResult.equals("goal"))
-    		result = RESULT_GOAL;
-    	else
-    	if(strResult.equals("out"))
-    		result = RESULT_OUT;   
-    	
-    	return result;
-    }
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -122,130 +85,118 @@ public class MainActivity extends Activity implements OnClickListener {
         butStart = (Button)findViewById(R.id.butStart); 
         butStart.setOnClickListener(this);
         
-        SetFeildPlace();
-                     
+        butStartFull = (Button)findViewById(R.id.butStartFull);
+        butStartFull.setOnClickListener(this);
+        
+        edNumPause = (EditText)findViewById(R.id.edNumPause);
+        
+        new Timer();
+        
+        SetFeildPlace();                                     
+    }     
+
+    
+    /**
+     * Единичный шаг игры
+     */
+    private void StartOneStep(){
+    	butStart.setClickable(false);
+    	butStartFull.setClickable(false);
+    	
+    	GamePost gPost = new GamePost(this);
+    	gPost.SetDrawGamePost(this);
+    	gPost.execute();
     }
     
-    private void drawIco(String result, String place) 
+    /**
+     * Начала полной игры
+     */
+    private void clickButStartFull()
     {   
+    	long pause = 1;
+    	
+    	resGame0 = 0;
+    	resGame1 = 0;
+    	
+    	CharSequence value = edNumPause.getText();
+    	if(!TextUtils.isEmpty(value)){    		
+    		pause = Integer.valueOf(value.toString());
+    	}
+    	pause = pause * 1000;
+    			
+    	GamePost gPost = new GamePost(this);	
+		gPost.SetDrawGamePost(this);
+		
+		TaskStartGame pt = new TaskStartGame();		
+		pt.setPause(pause);
+		pt.SetMaxGoal(MAX_SCORE);
+		pt.execute(gPost); 		
+    	
+    }
+    
+	@Override
+	public void onClick(View v) {
+		int cutId = v.getId();
+		
+		if(cutId == R.id.butStart){
+			StartOneStep();
+		}else
+		if(cutId == R.id.butStartFull){
+			clickButStartFull();
+		}		
+	}
+
+
+	@Override
+	public void onSuccessGamePost(int Place, int Result, int direction) {
     	if(curField != null){
     		curField.setImageBitmap(bacBitmap);
     	}
     	
-    	int iPlace = GetIntPlace(place);    
-    	int iResult = GetIntResult(result);
+		ArrayList<ImageView> feild_place = null;
     	
-    	if(iResult == RESULT_OUT)
-    		iPlace = PLACE_OUT;
-    	
-    	ArrayList<ImageView> feild_place = null;
-    	
+    	// --- Выесняем игровые области 
+
     	if(direction == 0){
     		feild_place = this.feild_place_0;
     		
-    		if(iResult == RESULT_GOAL){
+    		if(Result == GamePost.RESULT_GOAL){
     			this.resGame0++;
     		}   
     	}
     	else 
     	if(direction == 1){
     		feild_place = this.feild_place_1;
-    		if(iResult == RESULT_GOAL){
+    		if(Result == GamePost.RESULT_GOAL){
     			this.resGame1++;    	
     		}
     	}
-    		
-    	ImageView iv = feild_place.get(iPlace);    	
-    	curField = iv;
+    		   	
+    	curField = feild_place.get(Place); 
     	bacBitmap = ((BitmapDrawable)curField.getDrawable()).getBitmap();
 			
         int	iImgResult = R.drawable.ball;
     	
-    	switch (iResult) {
-			case RESULT_OUT:
+        // --- Берем нужную иконку под результат удара
+    	switch (Result) {
+			case GamePost.RESULT_OUT:
 				iImgResult = R.drawable.is_out;
 				break;
-			case RESULT_GOAL:
+			case GamePost.RESULT_GOAL:
 				iImgResult = R.drawable.is_goal;
 				break;
 			default:
 				iImgResult = R.drawable.ball;
 		}
     	
-    	iv.setImageResource(iImgResult);
+    	curField.setImageResource(iImgResult);
     	
-    	if(iResult == RESULT_GOAL){
+    	if(Result == GamePost.RESULT_GOAL){
     		String str = "Счет " + String.valueOf(resGame0) + ":" + String.valueOf(resGame1);
     		resGame.setText(str);
-    	}
-    		
-    }
-    
-    private Response.Listener<JSONObject> createMyReqSuccessListener() {
-        return new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                	String result = response.getString("result");
-                	String place = response.getString("place");
-                	
-                    if (!result.equals("goal")) {
-	                    if (direction == 1) {
-	                    	direction = 0;
-	                    } else {
-	                    	direction = 1;
-	                    }
-                    }
-                   
-                    String msg = String.valueOf(direction) + " (" + result + ") " + place;
-                    Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();                                       
-                    
-                    drawIco(result, place);
-                    
-                    butStart.setClickable(true);
-                } catch (JSONException e) {
-                	Toast.makeText(MainActivity.this, "Parse error", Toast.LENGTH_LONG).show();
-                }
-            }
-        };
-    }
-    
-    
-    private Response.ErrorListener createMyReqErrorListener() {
-        return new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-            	Toast.makeText(MainActivity.this, "Error = " + error.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        };
-    }
-    
-    private void clickButStart(){
-    	butStart.setClickable(false);
-		RequestQueue queue = VolleyHelper.getRequestQueue();
-		JSONObject object = new JSONObject();
-		try {
-			object.put("direction", direction);
-			JsonObjectRequest volleyRequest = new JsonObjectRequest(Method.POST,
-					"http://volley.sibext.com/",
-					object,
-					createMyReqSuccessListener(),
-					createMyReqErrorListener());
-			
-			queue.add(volleyRequest);
-			
-			
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}    	
-    }
-    
-	@Override
-	public void onClick(View v) {
-		if(v.getId() == R.id.butStart){
-			clickButStart();
-		}
-		
+    	}	
+    	
+    	butStart.setClickable(true);
+    	butStartFull.setClickable(true);    	
 	}
-
 }
